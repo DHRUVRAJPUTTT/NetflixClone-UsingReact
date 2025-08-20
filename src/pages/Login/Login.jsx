c9edd5b2b2e38965667f6a3d3bfa008cbacafe49
DHRUVRAJPUTTT <dhruvrajput3177@gmail.com>
2025-08-20 19:13:24 +0530

    Changes in login page and beginning to add database and api

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 const Login = () => {
+  const [signState, setSignState] = useState("Sign In")
+ { /*Importing useState from React Library*/}
   return (
     <div className='login'>
-      <img src={logo}  className='login-logo'alt="" />
+      <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form >
-          <input type="text" placeholder='Your Name' />
-           <input type="email" placeholder='Email' />
-            <input type="password" placeholder='Password' />
-            <button>Sign Up</button>
-            <div className="form-help">
-              <div className="remember">
-                <input type="checkbox" />
-                <label htmlFor="">Remember Me </label>
-              </div>
-              <p>Need Help?</p>
+         { /*Adding the feature that remove Your name placeholder if the state is sign in  */}
+          {signState === "Sign Up"? <input type="text" placeholder='Your Name' /> : <></>}
+         
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+          {/*We need to put signState in curly braces because 
+           anything inside tags is treated as a regular string 
+           unless you wrap it in curly braces*/}
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me </label>
             </div>
-            
+            <p>Need Help?</p>
+          </div>
+
 
 
 
 
         </form>
-
+        <div className="form-switch">
+          {/*Showing Sign up and new to netflix in Sign In panel by using ternary operator*/ }
+          {signState=== "Sign In"?
+          <p>New to Netflix? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now</span></p> :
+          <p>Already have a Account? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now</span></p>
+                                                                  }
+        </div>
       </div>
     </div>
   )
